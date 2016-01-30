f6d3b9b41dace8e5fbdf449dad8e1be17690310a
Gerald Schenke <realazrael@gmx.de>
2016-01-30 02:25:51 +0100

    added generic soundplayer

---
+++ Assets/scripts/SoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundPlayer : MonoBehaviour {
+    private AudioSource audioSource;
+
+    public static SoundPlayer instance;
+    public AudioClip sound1;
+
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound1() {
+        audioSource.PlayOneShot(sound1);
+    }
+
+}
