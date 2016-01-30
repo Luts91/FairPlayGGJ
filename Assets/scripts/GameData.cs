c2a260c55870883813302a64a8fc674dc15749c4
Gerald Schenke <realazrael@gmx.de>
2016-01-30 03:27:27 +0100

    added font, main menu and a undestroyable gamedatasingleton

---
+++ Assets/scripts/GameData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData : MonoBehaviour {
+    public static GameData instance;
+
+    public string[] winners = new string[4];
+    public bool[] cheated = new bool[4];
+
+    void Awake() {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            QuitGame();
+        }
+    }
+	public void StartGame() {
+        // TODO fade out/in
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame() {
+        Debug.Log("Goodbye!");
+        // TODO fade out
+        Application.Quit();
+    }
+}
