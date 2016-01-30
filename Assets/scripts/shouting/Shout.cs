d4d87e984fc3fb291f8c77bcc4ccc6402569dba9
Lutz Schönfelder <lutz.schoenfelder@gmail.com>
2016-01-30 01:54:54 +0100

    Shouting

---
+++ Assets/scripts/shouting/Shout.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shout : MonoBehaviour {
+	public Transform shoutOmeter;
+
+	public int character=0;
+	public float strength=0;
+	public float timer=0,maxTimer=5;
+
+
+	public float AIStrength=0.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (character==0){
+			if (Input.GetMouseButtonDown(0)){
+				strength+=1;
+			}
+		}else{
+			strength+=Random.value*AIStrength;
+		}
+
+		shoutOmeter.localScale=new Vector3(1,1,1)*strength;
+
+		timer+=Time.deltaTime;
+		if (timer>=maxTimer){
+			ShoutController.sc.RatingPhase(strength);
+			this.enabled=false;
+		}
+	
+	}
+
+	void OnEnable(){
+		strength=0;
+		timer=0;
+	}
+}
---
+++ Assets/scripts/shouting/ShoutController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ShoutController : MonoBehaviour {
+
+	public static ShoutController sc;
+	public GameObject playerRatingSelection;
+
+	public Shout shouter;
+
+	public bool shoutingPhase=true;
+
+	public Text ratingText;
+
+
+	public int[] ratings= new int[4];
+	public float sympathy=10;
+
+	public float maxStrength=40;
+
+	public List<int> played=new List<int>();
+	public int currentCharacter=0;
+	bool playerRated=false;
+
+	public float nextRoundTimer=0,nextRoundTime=4;
+
+	// Use this for initialization
+	void Start () {
+		sc=this;
+		ChooseCharacter();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!shoutingPhase && ((currentCharacter==0) || playerRated)){
+			nextRoundTimer+=Time.deltaTime;
+			if (nextRoundTimer>=nextRoundTime){
+				nextRoundTimer=0;
+				playerRated=false;
+
+				if (played.Count<5)
+					ChooseCharacter();
+			}
+		}
+	}
+
+	public void ChooseCharacter(){
+		do{
+			currentCharacter=Random.Range(0,5);
+		}while(played.Contains(currentCharacter));
+		played.Add(currentCharacter);
+
+		shoutingPhase=true;
+		shouter.enabled=true;
+		shouter.character=currentCharacter;
+	}
+
+	public void RatingPhase(float strength){
+		if (currentCharacter!=0)
+			playerRatingSelection.SetActive(true);
+
+
+		shoutingPhase=false;
+		string text="";
+		float rating;
+
+		int start=0;
+
+		if (currentCharacter!=0){
+			start=1;
+			ratings[0]=0;
+		}
+
+		for (int i=start; i<4; i++){
+			rating=(strength/maxStrength)*10-Random.value*(10-sympathy);
+			rating=Mathf.Clamp(rating,0,10);
+			ratings[i]=(int)rating+1;
+
+			text+=ratings[i]+" ";
+		}
+
+		ratingText.text=text;
+	}
+
+	public void PlayerRate(int rating){
+		playerRatingSelection.SetActive(false);
+		ratings[0]=rating;
+		playerRated=true;
+
+		ratingText.text=ratings[0]+" "+ratings[1]+" "+ratings[2]+" "+ratings[3];
+	}
+}
